464516725c729b8aee7222886719d08ed55bb6b9
Ronaldo Padilha <ronaldocp762@gmail.com>
2026-09-23 22:41:21 -0300

    Organiza classe Equipamento

--- GestaoDeEquipamentos.WebApp/Modulos/Equipamentos/Dominio/Equipamentos.cs
+++ GestaoDeEquipamentos.WebApp/Modulos/Equipamentos/Dominio/Equipamentos.cs
@@ -9,11 +9,14 @@ public sealed class Equipamento : EntidadeBase
     public DateTime DataFabricacao { get; set; }
     public Fabricante Fabricante { get; set; } = null!;
 
-    public Equipamento()
-    {
-    }
+    public Equipamento() { }
 
-    public Equipamento(string nome, decimal precoAquisicao, DateTime dataFabricacao, Fabricante fabricante)
+    public Equipamento(
+        string nome,
+        decimal precoAquisicao,
+        DateTime dataFabricacao,
+        Fabricante fabricante
+    )
     {
         Nome = nome;
         PrecoAquisicao = precoAquisicao;
